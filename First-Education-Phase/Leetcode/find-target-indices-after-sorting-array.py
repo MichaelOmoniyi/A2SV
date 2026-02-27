ae74bfe7f6ccc1bdd10d7239df3adbffd4483c7c
MichaelOmoniyi <oomoniyimicheal18@gmail.com>
2026-02-27 09:51:17 +0100

    Update problem solution using Insertion sort

--- First-Education-Phase/Leetcode/find-target-indices-after-sorting-array.py
+++ First-Education-Phase/Leetcode/find-target-indices-after-sorting-array.py
@@ -33,7 +33,15 @@
 
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
-        nums.sort()
+        for i in range(1, len(nums)):
+            key = nums[i]
+            j = i - 1
+            
+            while j >= 0 and key < nums[j]:
+                nums[j+1] = nums[j]
+                j -= 1
+            nums[j+1] = key
+
         res=[]
         for i in range(len(nums)):
             if nums[i]==target:
